1855769911c6daa3e3061cf7cd5d921d6cc453de
Пардаев Азиз <azikpardaev@mail.ru>
2023-06-08 19:06:16 +0300

    🐛 task 1 minor fix

--- task1.py
+++ task1.py
@@ -19,13 +19,13 @@ info_lines = all_file[:3][[0, 1]] # первые 3 строки
 data_table = all_file[4:]
 data_table.columns = data_table.iloc[0]
 data_table = data_table[1:].reset_index(drop=True) # таблица с данными
-data_table['IE Code'] = "0" + data_table['IE Code'].map(str) # пропадает leading 0 при чтении
+data_table['IE Code'] = '0' + data_table['IE Code'].map(str) # пропадает leading 0 при чтении
 data_table['SB Amount'] = data_table['SB Amount'].map(str)
 data = data_table.to_dict('records') # список словарей, чтобы быстрее итерироваться по данным
 
 root = ET.Element('CERTDATA') # формирование структуры xml
 filename = ET.SubElement(root, 'FILENAME')
-filename.text = 'SABR000000119042023002' # TODO
+filename.text = info_lines[1][2]
 envelope = ET.SubElement(root, 'ENVELOPE')
 
 for item in data:
